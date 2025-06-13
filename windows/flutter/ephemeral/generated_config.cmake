7c1481d728e4212ea3b193185622194651bccab3
lucresNova9393 <61306784+lucresNova9393@users.noreply.github.com>
2025-06-13 01:10:01 +0100

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\DELL\\Documents\\flutter_windows_3.29.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\DELL\\StudioProjects\\fuzz_movies" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\DELL\\Documents\\flutter_windows_3.29.3-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\DELL\\StudioProjects\\fuzz_movies"
+  "FLUTTER_ROOT=C:\\Users\\DELL\\Documents\\flutter_windows_3.29.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\DELL\\StudioProjects\\fuzz_movies\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\DELL\\StudioProjects\\fuzz_movies"
+  "FLUTTER_TARGET=C:\\Users\\DELL\\StudioProjects\\fuzz_movies\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\DELL\\StudioProjects\\fuzz_movies\\.dart_tool\\package_config.json"
+)
